88c34fef58a42b7618a8a4b799777a60681d5893
Geomar Perales <per3app@gmail.com>
2018-10-15 00:28:27 -0500

    Add files via upload

---
+++ Ari-Lamstein-Course/setup.R
@@ -0,0 +1,9 @@
+# to get the latest version of R, google "download latest version of R"
+# to get RStudio, visit rstudio.com
+
+R.Version()
+
+install.packages(c("choroplethr", "choroplethrMaps"))
+
+library(choroplethr)
+library(choroplethrMaps)
