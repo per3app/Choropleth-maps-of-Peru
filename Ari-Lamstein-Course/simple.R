32dcffabc5d5c70bb54a4b96af802de5813ca1c2
Geomar Perales <per3app@gmail.com>
2018-10-15 00:57:22 -0500

    Add files via upload

--- Ari-Lamstein-Course/simple.R
+++ Ari-Lamstein-Course/simple.R
@@ -1,3 +1,4 @@
+#
 library(choroplethr)
 library(choroplethrMaps)
 
@@ -10,3 +11,13 @@ state_choropleth(df_pop_state)
 
 state_choropleth(df_pop_state, title="2012 State Population Estimates",
                  legend = "Population")
+
+file<-'d:/geomar/modulos/programacion/R/Choroplethr-with-R'
+txt<-'data.txt'
+csv<-'data.csv'
+
+dir_file<-paste(file,txt,sep = '/')
+csv_file<-paste(file,csv,sep = '/')
+
+write.table(data,dir_file) #txt
+write.csv(data,csv_file) #csv
